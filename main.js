1e51e0fb611ddd374b450b9196c68ca70958d3cd
Diana Arita <dianaarita500@gmail.com>
2021-07-29 21:55:02 -0500

    Adding local files

---
+++ main.js
@@ -0,0 +1,205 @@
+async function init() {
+    // set the dimensions and margins of the graph
+    var width = 600;
+    var height = 600;
+    var margin = 50;
+
+    var x = d3.scaleLog()
+        .base(10)
+        .domain([10,150])
+        .range([0, width]);
+
+    var y = d3.scaleLog()
+        .domain([10,150])
+        .range([width, 0]);
+
+    var xAxis = d3.axisBottom(x)
+    .tickValues([10,20,50,100])
+    .tickFormat(d3.format("~s"));
+
+    var yAxis = d3.axisLeft(y)
+    .tickValues([10,20,50,100])
+    .tickFormat(d3.format("~s"));
+
+    const colorLegendG = d3.select("body").append('g')
+        .attr('transform', `translate(${innerWidth + 60}, 150)`);
+
+    const colorValue = d => d.Fuel;
+
+    const colorScale = d3.scaleOrdinal()
+        .range(d3.schemeSet1);
+
+    //Read the data
+    const data = await d3.csv("https://flunky.github.io/cars2017.csv");
+
+    var keys = [];
+
+    var div = d3.select("body").append("div")	
+        .attr("class", "tooltip")				
+        .style("opacity", 0);
+    
+    // Add dots
+    d3.select(".main-chart").append('g')
+        .attr("transform", "translate(50,50)")
+        .selectAll("dot")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("class", "cir")
+        .attr("cx", function (d,i) { keys[i] = d.Fuel; return x(d.AverageCityMPG); } )
+        .attr("cy", function (d) { return y(d.AverageHighwayMPG); } )
+        .attr("r", function (d) { return 2 + parseInt(d.EngineCylinders);})
+        .attr('fill', d => colorScale(colorValue(d)))
+        .attr('fill-opacity', 0.6)
+        .on("mouseover", function(d) {	
+            div.selectAll("*").remove();	
+            div.transition()		
+                .duration(200)		
+                .style("opacity", .9);		
+            div.text("Make: " + d.Make)
+                .style("left", (x(d.AverageCityMPG) + margin) + "px")		
+                .style("top", (y(d.AverageHighwayMPG) + margin) + "px");
+        })					
+        .on("mouseout", function(d) {		
+            div.transition()		
+                .duration(500)		
+                .style("opacity", 0);	
+        })
+        .on('click', function(d,i){
+            d3.select(".pop-up").selectAll("*").remove();
+
+            d3.select(".pop-up").append('g')
+                .append("div")
+                .attr("x", 10)
+                .attr("y", 70)
+                .style("width", "20px")
+                .style("width", "20px")
+                .attr("class", "close")
+                .attr("width", "20px")
+                .attr("height", "20px")
+                .style("background-color", "white")
+                .style("text-align", "center")
+                .style("margin-left", "90%")
+                .style("margin-bottom", "10px")
+                .text("X")
+                .on('click', function(d,i){
+                    d3.select(".pop-up").style("display","none");
+                });
+            
+            d3.select(".pop-up").append('g')
+                .attr("class", "pop-up-header")
+                .attr("x", 300)
+                .attr("y", 300)
+                .attr("dy", "0em")
+                .text("Detailed Information")
+                .style("color","black")
+                .style("display","block");
+
+            d3.select(".pop-up").append('g')
+                .attr("x", 300)
+                .attr("y", 300)
+                .attr("dy", "1em")
+                .text("Make: " + d.Make)
+                .style("color","black")
+                .style("display","block");
+
+            d3.select(".pop-up").append('g')
+                .attr("x", 300)
+                .attr("y", 300)
+                .attr("dy", "2em")
+                .text("Fuel: " + d.Fuel)
+                .style("color","black")
+                .style("display","block");
+
+            d3.select(".pop-up").append('g')
+                .attr("x", 300)
+                .attr("y", 300)
+                .attr("dy", "3em")
+                .text("Engine Cylinders: " + d.EngineCylinders)
+                .style("color","black")
+                .style("display","block");
+
+            d3.select(".pop-up").append('g')
+                .attr("x", 300)
+                .attr("y", 300)
+                .attr("dy", "4em")
+                .text("Average Highway MPG: " + d.AverageHighwayMPG)
+                .style("color","black")
+                .style("display","block");
+
+            d3.select(".pop-up").append('g')
+                .attr("x", 300)
+                .attr("y", 300)
+                .attr("dy", "5em")
+                .text("Average City MPG: " + d.AverageCityMPG)
+                .style("color","black")
+                .style("display","block");
+
+            d3.select(".pop-up").style("display","inline-block");
+        });
+    
+    var uniqueKeys = [];
+    $.each(keys, function(i, el){
+        if($.inArray(el, uniqueKeys) === -1) uniqueKeys.push(el);
+    });
+
+    var color = d3.scaleOrdinal()
+        .range(d3.schemeSet1);
+    
+    d3.select(".legend").append("g")
+        .append("text")
+        .text("Fuel Type")
+        .attr("x", 10)
+        .attr("y", 70)
+        .style("font-size","18px")
+        .style("font-weight","bold");
+
+    // Add one dot in the legend for each name.
+    d3.select(".legend").selectAll("mydots")
+    .data(uniqueKeys)
+    .enter()
+    .append("circle")
+        .attr("cx", 20)
+        .attr("cy", function(d,i){ return 100 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
+        .attr("r", 7)
+        .style("fill", function(d){ return color(d)});
+
+    // Add one dot in the legend for each name.
+    d3.select(".legend").selectAll("mylabels")
+    .data(uniqueKeys)
+    .enter()
+    .append("text")
+        .attr("x", 40)
+        .attr("y", function(d,i){ return 100 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
+        .style("fill", function(d){ return color(d)})
+        .text(function(d){ return d})
+        .attr("text-anchor", "left")
+        .style("alignment-baseline", "middle");
+
+    d3.select(".main-chart")
+        .append("g")
+            .attr("transform", "translate(" + margin + "," + margin + ")")
+            .call(yAxis);
+
+    d3.select(".main-chart")
+        .append("g")
+            .attr("transform", "translate(" + margin + "," + (height + margin) + ")")
+            .call(xAxis);
+    
+    d3.select(".main-chart")
+        .append("text")
+        .attr("class", "x-label")
+        .attr("text-anchor", "end")
+        .attr("x", width - 75)
+        .attr("y", height + 100)
+        .text("Average City MPG");
+
+    d3.select(".main-chart")
+        .append("text")
+        .attr("class", "y-label")
+        .attr("text-anchor", "end")
+        .attr("y", 0)
+        .attr("dy", ".75em")
+        .attr("transform", "translate(0,150)rotate(-90)")
+        .text("Average Hightway MPG");
+}
